a604e1d54eea0e5f2797eb9feda8968a8d78de25
ZiGmaX809 <pyrogas7@gmail.com>
2022-03-28 19:45:33 +0800

    update

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,8 +1,8 @@
-import { builtinModules } from 'module'
-import { defineConfig, Plugin } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import resolve from 'vite-plugin-resolve'
-import pkg from '../../package.json'
+import { defineConfig, Plugin } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import resolve from 'vite-plugin-resolve';
+import pkg from '../../package.json';
+import electron from 'vite-plugin-electron-renderer';
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
@@ -12,16 +12,18 @@ export default defineConfig({
   root: __dirname,
   plugins: [
     vue(),
-    resolveElectron(),
-    /**
-     * Here you can specify other modules
-     * 🚧 You have to make sure that your module is in `dependencies` and not in the` devDependencies`,
-     *    which will ensure that the electron-builder can package it correctly
-     * @example
-     * {
-     *   'electron-store': 'const Store = require("electron-store"); export default Store;',
-     * }
-     */
+    electron(),
+    resolve(
+      /**
+       * Here you can specify other modules
+       * 🚧 You have to make sure that your module is in `dependencies` and not in the` devDependencies`,
+       *    which will ensure that the electron-builder can package it correctly
+       */
+      {
+        // If you use electron-store, this will work
+        'electron-store': 'const Store = require("electron-store"); export default Store;',
+      }
+    ),
     Components({
       resolvers: [AntDesignVueResolver()],
     }),
@@ -32,81 +34,7 @@ export default defineConfig({
     outDir: '../../dist/renderer',
   },
   server: {
-    port: pkg.env.PORT,
+    host: pkg.env.VITE_DEV_SERVER_HOST,
+    port: pkg.env.VITE_DEV_SERVER_PORT,
   },
 });
-
-/**
- * For usage of Electron and NodeJS APIs in the Renderer process
- * @see https://github.com/caoxiemeihao/electron-vue-vite/issues/52
- */
-export function resolveElectron(
-  resolves: Parameters<typeof resolve>[0] = {}
-): Plugin[] {
-  const builtins = builtinModules.filter((t) => !t.startsWith('_'))
-
-  /**
-   * @see https://github.com/caoxiemeihao/vite-plugins/tree/main/packages/resolve#readme
-   */
-  return resolve({
-    electron: electronExport(),
-    ...builtinModulesExport(builtins),
-    ...resolves,
-  })
-
-  function electronExport() {
-    return `
-/**
- * For all exported modules see https://www.electronjs.org/docs/latest/api/clipboard -> Renderer Process Modules
- */
-const electron = require("electron");
-const {
-  clipboard,
-  nativeImage,
-  shell,
-  contextBridge,
-  crashReporter,
-  ipcRenderer,
-  webFrame,
-  desktopCapturer,
-  deprecate,
-} = electron;
-
-export {
-  electron as default,
-  clipboard,
-  nativeImage,
-  shell,
-  contextBridge,
-  crashReporter,
-  ipcRenderer,
-  webFrame,
-  desktopCapturer,
-  deprecate,
-}
-`
-  }
-
-  function builtinModulesExport(modules: string[]) {
-    return modules
-      .map((moduleId) => {
-        const nodeModule = require(moduleId)
-        const requireModule = `const M = require("${moduleId}");`
-        const exportDefault = `export default M;`
-        const exportMembers =
-          Object.keys(nodeModule)
-            .map((attr) => `export const ${attr} = M.${attr}`)
-            .join(';\n') + ';'
-        const nodeModuleCode = `
-${requireModule}
-
-${exportDefault}
-
-${exportMembers}
-`
-
-        return { [moduleId]: nodeModuleCode }
-      })
-      .reduce((memo, item) => Object.assign(memo, item), {})
-  }
-}
